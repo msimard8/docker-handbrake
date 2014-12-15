e0769bfb4fdcc9b07fb2a763c3125c67e974a296
Michael Simard <msimard8@gmail.com>
2014-12-14 20:15:27 -0800

    fixed the remove path:

--- watch.sh
+++ watch.sh
@@ -10,7 +10,7 @@ inotifywait -m /watched -e create -e moved_to |
                   echo "file finished copying to directory"
   	          destinationFileName="${file%.*}.mp4"; 
 	          eval "HandBrakeCLI -i "/watched/"'"$file"' -o "/destination/"'"$destinationFileName"' --preset="Universal""
-	          rm "$file"
+	          rm "$path""$file"
                done  
 	fi
     done
