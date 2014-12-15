7a065839453d9db838c72bdadeed022f016c892d
Michael Simard <msimard8@gmail.com>
2014-12-14 19:49:48 -0800

    once file appears in watch directory,wait for the close event for inotifywait to signal the copy has been completed

--- watch.sh
+++ watch.sh
@@ -5,10 +5,12 @@ inotifywait -m /watched -e create -e moved_to |
         echo "The file '$file' appeared in directory '$path' via '$action'"
 	if [[ $file == *.mkv ]] || [[ $file == *.avi ]]
 	   then
-	     destinationFileName="${file%.*}.mp4"; 
-	     eval "HandBrakeCLI -i '"$file"' -o "/destination/"'"$destinationFileName"' --preset="Universal""
-	     rm "$file"
+  	       inotifywait "$file" -e close | 
+               while read ; do 
+                  echo "file finished copying to directory"
+  	          destinationFileName="${file%.*}.mp4"; 
+	          eval "HandBrakeCLI -i '"$file"' -o "/destination/"'"$destinationFileName"' --preset="Universal""
+	          rm "$file"
+               done  
 	fi
-
-        # do something with the file
     done
