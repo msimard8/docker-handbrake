320442ff716ded4239eff01986d79473913a23f7
Michael Simard <msimard8@gmail.com>
2014-12-14 18:33:55 -0800

    cleanup:

--- watch.sh
+++ watch.sh
@@ -5,9 +5,7 @@ inotifywait -m /watched -e create -e moved_to |
         echo "The file '$file' appeared in directory '$path' via '$action'"
 	if [[ $file == *.mkv ]] || [[ $file == *.avi ]]
 	   then
-	     echo "$file "" mkv appeared"	
 	     destinationFileName="${file%.*}.mp4"; 
-  	     #echo $destinationFileName; 
 	     eval "HandBrakeCLI -i '"$file"' -o "/destination/"'"$destinationFileName"' --preset="Universal""
 	     rm "$file"
 	fi
