92f7879b0494368eb558d9727ff958ffcd991a9d
Michael Simard <msimard8@gmail.com>
2014-12-15 09:54:41 -0800

    changed way to wait for file copy to finish to use lsof

--- watch.sh
+++ watch.sh
@@ -5,12 +5,13 @@ inotifywait -m /watched -e create -e moved_to |
         echo "The file '$file' appeared in directory '$path' via '$action'"
 	if [[ $file == *.mkv ]] || [[ $file == *.avi ]]
 	   then
-  	       inotifywait "/watched/""$file" -e close | 
-               while read ; do 
-                  echo "file finished copying to directory"
-  	          destinationFileName="${file%.*}.mp4"; 
-	          eval "HandBrakeCLI -i "/watched/"'"$file"' -o "/destination/"'"$destinationFileName"' --preset="Universal""
-	          rm "/watched/""$file"
-               done  
+		until ! [[ `lsof -- "$path""$file"` ]]; do
+                  echo "file in use"
+	          sleep 0.5;
+                done 
+                echo "file finished copying to directory"
+  	        destinationFileName="${file%.*}.mp4"; 
+	        eval "HandBrakeCLI -i "/watched/"'"$file"' -o "/destination/"'"$destinationFileName"' --preset="Universal""
+   	        rm "/watched/""$file"
 	fi
     done
