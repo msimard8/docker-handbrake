214ddf0f0872ea7638ede37806846727872dfc00
Michael Simard <msimard8@gmail.com>
2014-12-14 20:46:28 -0800

    Experiments

--- watch.sh
+++ watch.sh
@@ -5,12 +5,9 @@ inotifywait -m /watched -e create -e moved_to |
         echo "The file '$file' appeared in directory '$path' via '$action'"
 	if [[ $file == *.mkv ]] || [[ $file == *.avi ]]
 	   then
-  	       inotifywait "$path""$file" -e close | 
-               while read ; do 
                   echo "file finished copying to directory"
   	          destinationFileName="${file%.*}.mp4"; 
 	          eval "HandBrakeCLI -i "/watched/"'"$file"' -o "/destination/"'"$destinationFileName"' --preset="Universal""
 	          rm "$path""$file"
-               done  
 	fi
     done
